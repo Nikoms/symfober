f62912f5e1d1e3ea196baed84a2e45bba6fe3ec8
Nikoms <nikoms@gmail.com>
2014-02-16 20:33:20 +0100

    Youpie! On arrive à prendre les post en ajax!!

--- src/Acme/DemoBundle/Resources/public/js/todo/application.js
+++ src/Acme/DemoBundle/Resources/public/js/todo/application.js
@@ -1,3 +1,11 @@
 window.Posts = Ember.Application.create();
 
-Posts.ApplicationAdapter = DS.FixtureAdapter.extend();
+Posts.ApplicationAdapter  = DS.RESTAdapter.extend({
+    namespace: 'app_dev.php/api' //Comment faire pour garder le app_dev.php si
+});
+
+Posts.ApplicationSerializer = DS.RESTSerializer.extend({
+    normalizePayload: function(type, payload) {
+        return {posts:payload.entities};
+    }
+});
